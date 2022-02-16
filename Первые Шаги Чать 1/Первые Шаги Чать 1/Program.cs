c0d7b57c6d79970d3c667750f3f6aac99506116e
nazirkhon <temurovnazirhon@gmail.com>
2022-02-16 09:00:58 +0700

    Создание Графики Приложения

---
+++ Первые Шаги Чать 1/Первые Шаги Чать 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Первые_Шаги_Чать_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Возведение В Степень");
+            Console.WriteLine("_____________________\n");
+            Console.Write("Введите число:");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("В какую Степень поднять:");
+            int Stepen = Convert.ToInt32(Console.ReadLine());
+
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Первые Шаги Чать 1/Первые Шаги Чать 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Первые Шаги Чать 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Первые Шаги Чать 1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4cfaff90-fb4e-4b89-be67-0d541deaa1b1")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
